f108f2c4b501d329a272968778c64e20095fc65f
Muhammed  Ashfaq <87014346+MhammedAshfaq@users.noreply.github.com>
2025-07-20 13:33:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World! CI/CD");
+  res.send("Hello World! CI/CD updated");
 });
 
 app.get("/test", (req, res) => {
